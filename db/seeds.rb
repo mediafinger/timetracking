318f5bb075fdaa82a074286635ab8f18a446acc3
Andreas Finger <webmaster@mediafinger.com>
2015-07-06 15:26:05 +0200

    Create unique projects in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,33 @@
+# Exercise #10
+# Fill the seeds file with some project creation statements.
+# Create 25 projects with different names and different descriptions.
+
+# this file is: db/seeds.rb
+
+puts " --- Deleting all Projects --- "
+Project.destroy_all
+
+def name_generator
+  syllables = %w(ana isa ola mum sasa lola hihi)
+
+  "#{syllables.sample}#{syllables.sample}#{syllables.sample}"
+end
+
+
+25.times do |number|
+  Project.create!(
+    name: "My name #{number}",
+    description: name_generator
+  )
+end
+
+
+# to run the seeds, in the terminal enter:
+
+# rake db:seed
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
