2c2c6e686c6e40679de6b66a3ab5e562b9719cdf
Andreas Finger <webmaster@mediafinger.com>
2015-07-07 16:32:12 +0200

    Add tests for the validations of Entry and Project

---
+++ test/models/entry_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+
+class EntryTest < ActiveSupport::TestCase
+  def setup
+    @project   = Project.create(name: "Ironhack")
+    @arguments = {
+      project: @project,
+      hours: 1,
+      minutes: 20,
+      date: 1.day.ago
+    }
+  end
+
+  def test_create_entry
+    entry = Entry.create(@arguments)
+
+    assert_equal @project.id, entry.reload.project_id
+  end
+
+  #testing the validations
+
+  def test_create_entry_without_project_id
+    @arguments.delete(:project)
+
+    assert_raises(ActiveRecord::RecordInvalid, "Project can't be blank") do
+      Entry.create!(@arguments)
+    end
+  end
+
+  def test_create_entry_without_negative_hours
+    @arguments[:hours] = -5
+
+    assert_raises(ActiveRecord::RecordInvalid, "Hours must be greater than 0") do
+      Entry.create!(@arguments)
+    end
+  end
+
+  def test_create_entry_with_minutes_above_60
+    @arguments[:minutes] = 66
+
+    assert_raises(ActiveRecord::RecordInvalid, "Minutes must be less than 60") do
+      Entry.create!(@arguments)
+    end
+  end
+
+  def test_create_entry_without_date
+    @arguments.delete(:date)
+
+    assert_raises(ActiveRecord::RecordInvalid, "Date can't be blank") do
+      Entry.create!(@arguments)
+    end
+  end
+end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @name = "Ironhack"
+  end
+
+  def test_create_project
+    project = Project.create(name: @name)
+
+    assert_equal @name, project.reload.name
+  end
+
+  def test_create_project_with_same_name
+    Project.create(name: @name)
+
+    assert_raises(ActiveRecord::RecordInvalid, "Name has already been taken") do
+      Project.create!(name: @name)
+    end
+  end
 end
