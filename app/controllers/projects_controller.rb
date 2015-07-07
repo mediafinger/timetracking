190a6c022be5bcffc69ceb2dad7da81caef435c7
Andreas Finger <webmaster@mediafinger.com>
2015-07-07 13:59:38 +0200

    Add all the methods to the ProjectsController and add the views

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,48 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.last_created(10)
+  end
+
+  def show
+    @project = Project.find_by(id: params[:id]) ||
+      render_404(params)
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def edit
+    @project = Project.find_by(id: params[:id]) ||
+      render_404(params)
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.valid?
+      @project.save
+      redirect_to projects_path, notice: 'Project was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @project = Project.find_by(id: params[:id])
+
+    if @project.update_attributes(project_params)
+      redirect_to projects_path, notice: 'Project was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
+end
