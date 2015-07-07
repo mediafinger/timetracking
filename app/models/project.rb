355b54eb4283da46d3936fa4d78262a64b2a610e
Andreas Finger <webmaster@mediafinger.com>
2015-07-07 13:53:57 +0200

    Add validation and finder methods to Project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,18 @@
+# app/models/project.rb
+
 class Project < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+
+  def self.iron_find(date = Date.current)
+    order("name ASC").where("created_at > ?", date).limit(10)
+  end
+
+  def self.clean_old(date = 1.week.ago)
+    where("created_at < ?", date).destroy_all
+  end
+
+  def self.last_created(n)
+    # raise ArgumentError unless n.is_a? Integer && n.in(1..25)
+    order(created_at: :desc).limit(n)
+  end
 end
